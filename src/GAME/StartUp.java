d983b433219f58c7192eeb3c5c2a8a87b90a6880
dot <tgdwarf@gmail.com>
2016-02-25 14:13:46 +0100

    Added startup
    startup file

---
+++ src/GAME/StartUp.java
@@ -0,0 +1,14 @@
+package GAME;
+
+/**
+ * Created by dot on 25-02-2016.
+ */
+public class StartUp {
+    ConsolePrinter consolePrinter = new ConsolePrinter();
+    public void startUp(){
+        consolePrinter.printLogo();
+
+        consolePrinter.printStartInfo();
+
+    }
+}
